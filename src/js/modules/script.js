88ab1353ecc5557f834dbdd8354375601d78a395
Idiot <verruckterdrachen@gmail.com>
2022-03-05 14:19:36 +0300

    add pricing section

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -4,4 +4,31 @@ const search = document.querySelector('.header__search');
 searchToggle.addEventListener('click', () => {
 	search.classList.toggle('active');
 });
+
+const toggle = document.querySelector('input[type="checkbox"].toggle');
+toggle.addEventListener('click', (e) => {
+	const el = e.target;
+	const prev = el.previousElementSibling;
+	const next = el.nextElementSibling;
+	if (el.checked) {
+		next.classList.add('active');
+		prev.classList.remove('active');
+	} else {
+		prev.classList.add('active');
+		next.classList.remove('active');
+	}
+	const pricesItems = document.querySelectorAll('.item-pricing__price');
+	let prices;
+	let plan;
+	if (el.checked) {
+		prices = [25, 50, 75];
+		plan = 'Year';
+	} else {
+		prices = [50, 99, 150];
+		plan = 'Month';
+	}
+	pricesItems.forEach((pricesItem, index) => {
+		pricesItem.innerHTML = `$${prices[index]}<span>/${plan}</span>`;
+	});
+});
 //================================================================================
